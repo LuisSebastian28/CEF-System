efd61ff16f6fa81c00d97164249be96d62d2d392
Sebastian Salazar <sabestech@gmail.com>
2024-10-28 13:43:31 -0400

    CEFapp arreglado

--- src/CEFApp.jsx
+++ src/CEFApp.jsx
@@ -1,9 +1,4 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { onAuthStateChanged } from 'firebase/auth';
-import { FirebaseAuth } from './firebase/firebaseConfig';  // Asegúrate de importar correctamente FirebaseAuth
-import { login, logout } from './store/auth/authSlice';  // Importa tus acciones de autenticación
-import { getUserFromFirestore } from './firebase/provs/userProviders';  // Función para obtener más datos del usuario
+
 import { CefRouter } from './routes/CefRouter';
 import { useCheckOut } from './hooks/useCheckOut';
 
